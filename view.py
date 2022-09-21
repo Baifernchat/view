312b54ff3783fdaee9eb9e4f9d7286a51cb51b6b
Baifernchat <101575184+Baifernchat@users.noreply.github.com>
2022-09-21 16:25:31 +0700

    Update view.py

--- view.py
+++ view.py
@@ -1,6 +1,18 @@
 import tkinter as tk
 
+
+
+
+
 app = tk.Tk()
 app.title('Find')
 app.geometry('400x400')
+
+bs1 = tk.Label(master = app, text=" ")
+bs1.pack()
+title_1 = tk.Label(master = app, text="Enter point")
+title_1.pack()
+
+b1 = tk.Button(master = app, text="Enter point")
+b1.pack()
 app.mainloop()
